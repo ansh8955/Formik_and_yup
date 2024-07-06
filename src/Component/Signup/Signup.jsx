de33a1fe89a303a50854fbaac3cf9903cbbeef38
Aniket jauhri <aniketjauhri2003@gmail.com>
2024-07-06 22:53:28 +0530

    done

--- src/Component/Signup/Signup.jsx
+++ src/Component/Signup/Signup.jsx
@@ -1,34 +1,47 @@
 import { useFormik } from "formik";
 import { SignupSchemas } from "../../Schema";
+
 const initialValues = {
   name: "",
   email: "",
   password: "",
   confirm_password: "",
 };
+
 function Signup() {
-  const { values, touched, handleBlur, handleSubmit, handleChange, errors } =
-    useFormik({
-      initialValues: initialValues,
-      validationSchema: SignupSchemas,
-      onSubmit: (values, action) => {
-        console.log(values);
-        action.resetForm();
-      },
-    });
-  console.log(errors);
-  function checkallfields() {
+  const {
+    values,
+    touched,
+    handleBlur,
+    handleSubmit,
+    handleChange,
+    errors,
+    isValid,
+  } = useFormik({
+    initialValues: initialValues,
+    validationSchema: SignupSchemas,
+    onSubmit: (values, action) => {
+      console.log(values);
+      action.resetForm();
+    },
+  });
+
+  function checkAllFields() {
     return (
-      values.name && values.email && values.password && values.confirm_password
+      values.name &&
+      values.email &&
+      values.password &&
+      values.confirm_password &&
+      isValid
     );
   }
 
   return (
-    <div className="w-4/5  mx-auto my-12 border flex rounded-md bg-white">
-      <div className="w-1/2 p-20 border">
+    <div className="w-4/5 mx-auto my-9 border flex rounded-md bg-white ">
+      <div className="w-full lg:w-1/2 p-8 lg:p-20 border">
         <h1 className="text-xl text-red-900 font-extrabold mx-3">Welcome!</h1>
-        <form onSubmit={handleSubmit}>
-          <div className=" flex flex-col p-2 border text-sm m-3">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col p-2 border text-sm m-3">
             <label
               htmlFor="name"
               className="w-full text-xs text-red-900 font-bold"
@@ -43,13 +56,13 @@ function Signup() {
               value={values.name}
               onChange={handleChange}
               onBlur={handleBlur}
-              className="bg-white text-gray-400 outline-none"
+              className="bg-white text-black  outline-none px-4 py-2"
             />
-            {errors.name && touched.name ? (
+            {errors.name && touched.name && (
               <p className="text-xs text-red-800">{errors.name}</p>
-            ) : null}
+            )}
           </div>
-          <div className=" flex flex-col p-2 border text-sm m-3">
+          <div className="flex flex-col p-2 border text-sm m-3">
             <label
               htmlFor="email"
               className="w-full text-xs text-red-900 font-bold"
@@ -64,13 +77,13 @@ function Signup() {
               value={values.email}
               onChange={handleChange}
               onBlur={handleBlur}
-              className="bg-white text-gray-400 outline-none"
+              className="bg-white  text-black outline-none px-4 py-2"
             />
-            {errors.email && touched.email ? (
+            {errors.email && touched.email && (
               <p className="text-xs text-red-800">{errors.email}</p>
-            ) : null}
+            )}
           </div>
-          <div className=" flex flex-col p-2 border text-sm m-3">
+          <div className="flex flex-col p-2 border text-sm m-3">
             <label
               htmlFor="password"
               className="w-full text-xs text-red-900 font-bold"
@@ -85,57 +98,59 @@ function Signup() {
               value={values.password}
               onChange={handleChange}
               onBlur={handleBlur}
-              className="bg-white text-gray-400 outline-none"
+              className="bg-white text-black outline-none px-4 py-2"
             />
-            {errors.password && touched.password ? (
+            {errors.password && touched.password && (
               <p className="text-xs text-red-800">{errors.password}</p>
-            ) : null}
+            )}
           </div>
-          <div className=" flex flex-col p-2 border text-sm m-3">
+          <div className="flex flex-col p-2 border text-sm m-3">
             <label
               htmlFor="confirm_password"
               className="w-full text-xs text-red-900 font-bold"
             >
-              Confirm_password
+              Confirm Password
             </label>
             <input
               type="password"
               name="confirm_password"
               id="confirm_password"
-              placeholder="Confirm_password"
+              placeholder="Confirm Password"
               value={values.confirm_password}
               onChange={handleChange}
               onBlur={handleBlur}
-              className="bg-white text-gray-400 outline-none"
+              className="bg-white text-black outline-none px-4 py-2"
             />
-            {errors.confirm_password && touched.confirm_password ? (
+            {errors.confirm_password && touched.confirm_password && (
               <p className="text-xs text-red-800">{errors.confirm_password}</p>
-            ) : null}
+            )}
           </div>
 
           <div className="flex justify-center items-center">
             <button
-              disabled={!checkallfields()}
+              disabled={!checkAllFields()}
               type="submit"
               className={`${
-                !checkallfields()
-                  ? "bg-[#ccc] cursor-not-allowed "
+                !checkAllFields()
+                  ? "bg-[#ccc] cursor-not-allowed"
                   : "bg-[#55311c] cursor-pointer"
-              } w-1/3`}
+              } w-2/3 lg:w-1/3 text-white py-2 rounded-md`}
+              style={{ fontSize: "0.8rem" }}
             >
               Sign Up
             </button>
           </div>
         </form>
       </div>
-      <div className="rounded-tr-md rounded-br-md">
+      <div className="hidden lg:block rounded-tr-md rounded-br-md overflow-hidden">
         <img
           src="https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600"
           alt=""
-          className="h-full rounded-tr-md rounded-br-md"
+          className="h-full"
         />
       </div>
     </div>
   );
 }
+
 export default Signup;
